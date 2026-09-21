8ba090caf496f20a8b44a73465ac6fcbe018ab3d
SandraJonsson73 <stallnysater@gmail.com>
2026-09-21 17:59:02 +0200

    Snyggat till lite i program.cs

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins("http://localhost:5173")
-              .AllowAnyMethod()
-              .AllowAnyHeader();
+            .AllowAnyMethod()
+            .AllowAnyHeader();
     });
 });
 
